a2089426263c3922b061eec065df89f1fc607952
shreejiravi <ravipatel@shreejiweb.com>
2019-01-18 16:41:58 +0530

    d

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:app_watcher/screens/note_list.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Sqflite demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NoteList(),
+    );
+  }
+}
